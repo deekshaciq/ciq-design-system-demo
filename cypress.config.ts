045efef33dd8f2d734ffa6338146391cf803281f
Deeksha Sharma <deeksha.sharma@creatoriq.com>
2024-10-24 13:43:19 +1300

    removed unused code from cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,20 +1,3 @@
-// import { defineConfig } from 'cypress';
-//
-// export default defineConfig({
-//     component: {
-//         devServer: {
-//             framework: 'react',
-//             bundler: 'vite',
-//         },
-//         specPattern: 'src/components/**/*.cy.{js,jsx,ts,tsx}',
-//         supportFile: 'cypress/support/component.tsx',
-//         videosFolder: 'cypress/videos',
-//         screenshotsFolder: 'cypress/screenshots',
-//     },
-//     viewportWidth: 1000,
-//     viewportHeight: 800,
-// });
-
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
